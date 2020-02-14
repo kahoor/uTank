51dbab1a473215e13a2daa8b29b7a4fcb5decb44
kahoor <amirr137@yahoo.com>
2020-02-14 17:42:26 +0330

    sth went wrong last time

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,283 +1,74 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.FocusListener;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
 
-public class GamePlay extends JPanel {
-
-    private List<Thing> everyThing = new ArrayList<>();
-    private List<Wall> walls = new ArrayList<>();
-    private List<Bullet> shotsInTheAir = new ArrayList<>();
-    private List<PowerUp> powerUpsInTheAir = new ArrayList<>();
-
-    private Player player2;
-    private Player player1;
-
-    private int WIDTH, HEIGHT, Max_ammo, GOAL;
-
-    GameActionListener listener;
-
-    private Random rand = new Random();
+public class GameFrame extends JFrame{
+    private Menu menu;
+    private GamePlay gameplay;
+    boolean started = false;
 
 
-    GamePlay(Player p1, Player p2, GameActionListener listener , int WIDTH, int HEIGHT, int max_ammo, int goal){
-        this.WIDTH = WIDTH-200;
-        this.HEIGHT = HEIGHT;
-        player1=p1;
-        player2=p2;
-//        player1.getTank().shots = max_ammo;
-//        player2.getTank().shots = max_ammo;
-        this.setGOAL(goal);
-        this.listener = listener;
-        this.setMax_ammo(max_ammo);
-        addWalls();
-        gameStart(false, false);
+
+
+    public GameFrame(Menu m,GamePlay g, int WIDTH, int HEIGHT){
+        this.setSize(WIDTH, HEIGHT);
+//        g.setMax_ammo(m.slider_ammo.getValue());
+        menu = m;
+        gameplay = g;
+//        gameplay.setMax_ammo(menu.slider_ammo.getValue());
+        this.setResizable(false);
+        gameplay.setVisible(true);
+        this.add(gameplay);
     }
 
-//    public boolean showGame(){ return this.listener.escape;}
-
-    void addWalls(){
-        try {
-            File map = new File("map1.txt");
-            Scanner reader = new Scanner(map);
-            while (reader.hasNextLine()) {
-                String data = reader.nextLine();
-                String[] coords = data.split(",");
-                this.walls.add(new Wall(Integer.parseInt(coords[0]),
-                        Integer.parseInt(coords[1]),
-                        Integer.parseInt(coords[2]),
-                        Integer.parseInt(coords[3])));
-            } reader.close();
-        } catch (FileNotFoundException e){
-            System.out.println("Map not found!");
-        }
-
-        Wall leftEdge = new Wall(20, 20, 1,HEIGHT - 60);
-        this.walls.add(leftEdge);
-        Wall topEdge = new Wall(20, 20, WIDTH - 40,1);
-        this.walls.add(topEdge);
-        Wall rightEdge = new Wall(WIDTH - 20, 20, 1, HEIGHT-60);
-        this.walls.add(rightEdge);
-        Wall bottomEdge = new Wall(20, HEIGHT - 40, WIDTH - 40,1);
-        this.walls.add(bottomEdge);
+    public void showMenu(){
+        this.remove(this.gameplay);
+        this.gameplay.setVisible(false);
+        this.menu.setVisible(true);
+        menu.checkContinue();
+        this.add(this.menu);
     }
 
-    private void gameStart(boolean p1win, boolean p2win) {
-        this.setBackground(Color.black);
-
-        System.out.println("setting tanks max ammo : " + this.getMax_ammo());
-
-        this.player1.newRound(p1win, rand.nextInt(WIDTH-100)+50,
-                rand.nextInt(HEIGHT-100)+50, Color.CYAN, this.getMax_ammo());
-        this.player2.newRound(p2win, rand.nextInt(WIDTH-100)+50,
-                rand.nextInt(HEIGHT-100)+50, Color.red, this.getMax_ammo());
-
-        this.everyThing.add(player1.getTank());
-        this.everyThing.add(player2.getTank());
+    public void showGamePlay() {
+        this.remove(this.menu);
+        this.menu.setVisible(false);
+        this.gameplay.setVisible(true);
+        this.gameplay.updateState();
+        this.add(this.gameplay);
+        gameplay.requestFocusInWindow();
     }
 
-    void chekPowerUp(){
-        String[] s = new String[2];
-        s[0] = "mine";
-        s[1] = "laser";
+    public void updateState(){
 
-        if (rand.nextInt(60000)<500){
-            addPowerUp(s[rand.nextInt(2)]);
-        }
-    }
 
-    void addPowerUp(String type){
-        PowerUp p = new PowerUp(rand.nextInt(WIDTH-60)+20, rand.nextInt(HEIGHT-60)+20, type);
 
-        powerUpsInTheAir.add(p);
-    }
-
-    void powerUpHandler(Tank t){
-        if (!(t.bulletType == "shot")){
-            t.powerUpGrowOld();
-            if(t.powerUpIsDead()){
-                t.bulletType = "shot";
-            }
-
-        }
-
-        for (PowerUp p: powerUpsInTheAir) {
-            if (p.contact(t)){
-                t.bulletType = p.powerUpType;
-                powerUpsInTheAir.remove(p);
-                t.tankPowerUpAge = 1000;
-                break;
-            }
+        if (this.menu.newGame) {
+            gameplay.setMax_ammo(menu.slider_ammo.getValue());
+            gameplay.setGOAL(menu.slider_goal.getValue());
+            this.menu.gameStarted = true;
+            this.menu.isClicked = true;
+            this.menu.newGame = false;
         }
 
 
+        if (this.gameplay.listener.escape){
 
-    }
-
-    void fireHandler(Tank t){
-        if (t.shots > 0) {
-            t.shots--;
-            if (t.bulletType == "shot") {
-
-                this.shotsInTheAir.add(new Shot(
-                        t.getGunX(), t.getGunY(), t.getDirection(), t, t.color
-                ));
-            }
-
-            if (t.bulletType == "laser") {
-                this.shotsInTheAir.add(new Laser(
-                        t.getGunX(), t.getGunY(), t.getDirection(), t
-                ));
-            }
-
-            if (t.bulletType == "mine") {
-                this.shotsInTheAir.add(new Mine(
-                        t.getGunX(), t.getGunY(), t
-                ));
-                t.bulletType = "shot";
-            }
+            this.showGamePlay();
         }
-
-
-
-
-    }
-
-    void tankAction(Tank p1Tank, Tank p2Tank){
-        if (listener.p1Move) {
-//        System.out.println("yeahhh");
-            p1Tank.step();
-
+        else this.showMenu();
+        if (this.menu.isClicked) {
+            this.gameplay.listener.escape = true;
+            this.menu.isClicked = false;
         }
-        if (this.walls.stream().anyMatch(wall -> wall.contacts(p1Tank)))
-            p1Tank.stepBack();
-
-        powerUpHandler(p1Tank);
-
-        if (listener.p1Left)
-            p1Tank.turnLeft();
-        if (listener.p1Right)
-            p1Tank.turnRight();
-        if (listener.p1Fire && !listener.p1shoot) {
-            listener.p1shoot = true;
-            fireHandler(p1Tank);
-
-        }
-
-
-        if (listener.p2Move )
-            p2Tank.step();
-        if (this.walls.stream().anyMatch(wall -> wall.contacts(p2Tank)))
-            p2Tank.stepBack();
-
-        powerUpHandler(p2Tank);
-
-        if (listener.p2Left)
-            p2Tank.turnLeft();
-        if (listener.p2Right)
-            p2Tank.turnRight();
-        if (listener.p2Fire && !listener.p2shoot) {
-            listener.p2shoot = true;
-            fireHandler(p2Tank);
-        }
-
-
-    }
-
-    void shootHandler(){
-        boolean p1win = false;
-        boolean p2win = false;
-
-        Tank t ;
-        for (Bullet shot: shotsInTheAir) {
-            for (Wall wall : this.walls) {
-                if (wall.contacts(shot) && (shot instanceof Shot)){
-                    shot.bounceAgainst(wall);
-                    while (wall.contacts(shot)){
-                        shot.step();
-                    }
-                }
-                shot.step();
-            }
-
-
-            if (shot.tank == this.player1.tank){
-                t = this.player2.tank;
-                p1win = true;
-            }
-            else {
-                t = this.player1.tank;
-                p2win = true;
-            }
-
-            if (shot instanceof Mine){
-                ((Mine) shot).visibility(t);
-            }
-
-            double distance = Math.sqrt( ((shot.x - t.x)*(shot.x - t.x)) + ((shot.y - t.y)*(shot.y - t.y)));
-
-            if ((distance <= (int) shot.getRADIUS()+ (int) t.getRADIUS())) {
-                everyThing.clear();
-                gameStart(p1win,p2win);
-                shotsInTheAir.clear();
-                powerUpsInTheAir.clear();
-                break;
-            }
+        if( gameplay.getPlayer1().points == menu.slider_goal.getValue() ||
+                gameplay.getPlayer2().points == menu.slider_goal.getValue()) {
+            this.gameplay.listener.escape = false;
+            this.menu.gameStarted = false;
+            gameplay.getPlayer1().points = 0;
+            gameplay.getPlayer2().points = 0;
         }
     }
 
+    void newGame(){
 
-    void updateState() {
-
-//        System.out.println("yeahhh");
-        this.shotsInTheAir.forEach(Bullet::growOld);
-        this.shotsInTheAir.removeIf(Bullet::isDead);
-
-        if (this.player1.getTank().shots > this.getMax_ammo()) this.player1.getTank().shots = this.getMax_ammo();
-        if (this.player2.getTank().shots > this.getMax_ammo()) this.player2.getTank().shots = this.getMax_ammo();
-        tankAction(this.player1.tank, this.player2.tank);
-
-
-        shootHandler();
-
-        chekPowerUp();
-
-        // ... handle other game actions
-    }
-
-    public int getMax_ammo(){ return this.Max_ammo; }
-    public void setMax_ammo( int maxAmmo ){ this.Max_ammo = maxAmmo; }
-
-    public int getGOAL(){ return this.GOAL; }
-    public void setGOAL( int GOAL ){ this.GOAL = GOAL; }
-
-    public Player getPlayer1(){ return this.player1; }
-
-    public Player getPlayer2(){ return this.player2; }
-
-    public void paint(Graphics graphics) {
-
-        graphics.setColor(Color.white);
-        super.paint(graphics);
-        graphics.setColor(Color.white);
-        graphics.drawString("Points to win : " + this.getGOAL(),1094, 40);
-        graphics.setColor(Color.CYAN);
-        graphics.drawString("Player 1 :", 1044, 90);
-        graphics.drawString(player1.points + " points", 1064, 110);
-        graphics.drawString(player1.getTank().shots + " shots left", 1064, 130);
-        graphics.setColor(Color.RED);
-        graphics.drawString(player2.points + " points", 1064, 410);
-        graphics.drawString("Player 2 :", 1044, 390);
-        graphics.drawString(player2.getTank().shots + " shots left", 1064, 430);
-        this.everyThing.forEach(thing -> thing.draw(graphics));
-        this.shotsInTheAir.forEach(shot -> shot.draw(graphics));
-        this.walls.forEach(wall -> wall.draw(graphics));
-        this.powerUpsInTheAir.forEach(shot -> shot.draw(graphics));
-        Toolkit.getDefaultToolkit().sync();
     }
 }
+
